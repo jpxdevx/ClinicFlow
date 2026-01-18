d1c929e0c3f58e999f67dda4e4181456b5eedf71
jp_devx <johnpaulfernandes18@gmail.com>
2026-01-19 00:30:37 +0530

    Appointment handling & Contact

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const port = 80;
 
 // Serving static files
 app.use('/static', express.static(path.join(__dirname, 'static')));
+// URL encoding
+app.use(express.urlencoded({extended: true}));
 
 // Pug
 app.set('view engine', 'pug');
@@ -30,10 +32,41 @@ app.get('/services', (req, res) =>
     res.status(200).render('services.pug', {serviceCards: services});
 });
 
+// Appointment form
 app.get('/appointment', (req, res) =>
 {
     res.status(200).render('appointment.pug');
-})
+});
+
+// Appointment form submission
+app.post('/submit-appointment', (req, res) =>
+{
+    // Read from the form
+    const email = req.body.email;
+
+    res.redirect(`/success?email=${encodeURIComponent(email)}`);
+});
+
+// Success page display
+app.get('/success', (req, res) =>
+{
+    // Read from the URL
+    const email = req.query.email;
+
+    res.status(200).render('success.pug', {mail: email});
+});
+
+// Contact
+app.get('/contact', (req, res) =>
+{
+    res.status(200).render('contact.pug');
+});
+
+// Learn more
+app.get('/about', (req, res) =>
+{
+    res.status(200).render('about.pug');
+});
 
 // Server
 app.listen(port, () => 
